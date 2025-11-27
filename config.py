b25c7da5ea20f971bd5b934c0ab5723a53e29d97
asralastnay <asralastnay@gmail.com>
2025-11-27 18:18:56 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+# إعدادات الاتصال بـ Green-API و Render
+
+# بيانات حسابك
+INSTANCE_ID = "7105395235"
+API_TOKEN = "7a7cf9442dbc4d9cb736b48c11ff9c5a077f22ed00fc465dbe"
+
+# اسم ملف البيانات
+JSON_FILE = "mainDataQuran.json"
+
+# إعدادات الرسائل
+MAX_MESSAGE_LENGTH = 7000  # طول الرسالة الواحدة
+DELAY_BETWEEN_PARTS = 3    # ثواني الانتظار بين الرسائل الطويلة
